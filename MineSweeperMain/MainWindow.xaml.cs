b438d7c3024fe9d4faf425bdb0b9935bcf079454
Justin V <dcarnold22@gmail.com>
2025-08-28 05:00:29 -0600

    Timer

--- MineSweeperMain/MainWindow.xaml.cs
+++ MineSweeperMain/MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
+
 namespace MineSweeperMain
 {
     /// <summary>
@@ -16,9 +18,36 @@ namespace MineSweeperMain
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer? gameTimer;
+        private int elapsedSeconds;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTimer();
+            StartTimer();
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            ResetTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            gameTimer = new DispatcherTimer();
+            gameTimer.Interval = TimeSpan.FromSeconds(1);
+            gameTimer.Tick += (s, e) => TimerLabel.Content = $"Time: {++elapsedSeconds:D3}";
+        }
+
+        public void StartTimer() => gameTimer.Start();
+        public void StopTimer() => gameTimer.Stop();
+        public void ResetTimer()
+        {
+            StopTimer();
+            elapsedSeconds = 0;
+            TimerLabel.Content = "Time: 000";
+            StartTimer();
         }
     }
 }
