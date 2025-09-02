8fbd841ea7e02c6c6affd3b752a1f70169ebdee7
Justin V <dcarnold22@gmail.com>
2025-09-02 00:49:56 -0600

    Sounds
    please dont error out

---
+++ Minesweeper/Pages/MainMenuPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Media;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Minesweeper.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainMenuPage.xaml
+    /// </summary>
+    public partial class MainMenuPage : Page
+    {
+        private Frame _mainFrame;
+        SoundPlayer bgm = new SoundPlayer(Properties.Resources.main_menu);
+
+        public MainMenuPage(Frame mainFrame)
+        {
+            InitializeComponent();
+            _mainFrame = mainFrame;
+            bgm.PlayLooping();
+        }
+
+        private void StartGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            _mainFrame.Navigate(new GameBoardPage(_mainFrame));
+            bgm.Stop();
+        }
+
+        private void Setting_Click(object sender, RoutedEventArgs e)
+        {
+            _mainFrame.Navigate(new SettingsPage(_mainFrame));
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
